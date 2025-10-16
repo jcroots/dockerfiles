a7575b6d1c4097e1f436bb80ccf64d44393b251a
Jeremías <jeremias.rootstrap@gmail.com>
2025-10-16 13:17:49 -0300

    debian forky docker workspace

--- debian/forky/run.sh
+++ debian/forky/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
+workspace="${HOME}/Workspace/debian"
+mkdir -vp "${workspace}"
 exec docker run -it --rm -u jrms \
     --name debian-forky \
-    --hostname forky.local \
+    --hostname forky.debian.local \
+    -v "${workspace}:/home/jrms/workspace" \
     jcroots/forky
