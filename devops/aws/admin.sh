1f2797262a69718b945fa2b66a656b2d02174af7
Jeremías <jeremias.rootstrap@gmail.com>
2025-10-16 15:49:26 -0300

    admin aws mount devops tools source dir

--- devops/aws/admin.sh
+++ devops/aws/admin.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -eu
+
 reponame=$(basename "${PWD}")
 workspace="${HOME}/Workspace/devops/${reponame}"
+
+devops_srcdir="${HOME}/Github/jcroots/devops/aws/opt/devops"
+
 mkdir -vp "${workspace}/docker/config/aws"
+
 exec docker run -it --rm -u admin \
     --name "admin-aws-${reponame}" \
     --hostname "${reponame}.admin-aws.local" \
-    -v "${PWD}/docker/config/aws:/home/admin/.config/aws" \
+    -v "${devops_srcdir}:/opt/devops" \
+    -v "${workspace}/docker/config/aws:/home/admin/.config/aws" \
     -v "${PWD}:/home/admin/src" \
     --workdir /home/admin/src \
     jcroots/admin-aws
