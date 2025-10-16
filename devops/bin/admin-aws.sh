f5a6cb08b0fccb415c5980d57165da748d6a7f11
Jeremías <jeremias.rootstrap@gmail.com>
2025-10-16 12:48:22 -0300

    devops admin aws script

---
+++ devops/bin/admin-aws.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+srcdir="$(dirname $(dirname $(realpath $0)))/aws"
+admin="${srcdir}/admin.sh"
+test -x "${admin}" || {
+	echo "${admin}: not found or not executable" >&2
+	exit 9
+}
+echo "${admin}"
+exec "${admin}"
