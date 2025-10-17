1fb36bd4cdb33e6dcb167c6dc228bc5fcf76bcf5
Jeremías <jeremias.rootstrap@gmail.com>
2025-10-17 10:58:25 -0300

    forky pass TERM env var

--- debian/forky/run.sh
+++ debian/forky/run.sh
@@ -5,5 +5,6 @@ mkdir -vp "${workspace}"
 exec docker run -it --rm -u jrms \
     --name debian-forky \
     --hostname forky.debian.local \
+    -e "TERM=${TERM}" \
     -v "${workspace}:/home/jrms/workspace" \
     jcroots/forky
