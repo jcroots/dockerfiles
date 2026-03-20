0c62a60bb924486742758f48ba026b3b45ffeda0
Jeremías <jeremias.rootstrap@gmail.com>
2026-03-20 13:42:00 -0300

    fix claude devops run script

--- claude/claude-devops/run.sh
+++ claude/claude-devops/run.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -eu
+
 user=$(id -u -n)
+
+workspace="${HOME}/Workspace"
+github="${HOME}/Github"
+
 cfgdir="${HOME}/Workspace/docker/dockerfiles/claude"
 install -v -d -m 0750 "${cfgdir}"
 install -v -d -m 0750 "${cfgdir}/config"
-if ! test -s "${cfgdir}/config/claude.json"; then
-	touch "${cfgdir}/config/claude.json"
+
+if ! test -s "${cfgdir}/claude.json"; then
+	touch "${cfgdir}/claude.json"
 fi
-slug="$(echo -n "$(basename "${PWD}")" | tr '[:space:]' '-')"
+
 exec docker run -it --rm -u "${user}" \
-	--name "claude-devops-${slug}" \
-	--hostname "claude-devops-${slug}.debian.local" \
+	--name "claude-devops-${user}" \
+	--hostname "claude-devops.debian.local" \
 	-e "TERM=${TERM}" \
 	-v "${cfgdir}/config:/home/${user}/.claude" \
 	-v "${cfgdir}/claude.json:/home/${user}/.claude.json" \
-	-v "${PWD}:/home/${user}/workspace" \
-	--workdir "/home/${user}/workspace" \
+	-v "${workspace}:/home/${user}/workspace:ro" \
+	-v "${github}:/home/${user}/github" \
+	--workdir "/home/${user}" \
 	jcroots/claude-devops
