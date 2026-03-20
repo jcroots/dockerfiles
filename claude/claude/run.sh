e87298c78efdd5bd3ae58b533fd70d9ab13a64d5
Jeremías <jeremias.rootstrap@gmail.com>
2026-03-20 13:28:36 -0300

    claude git disable commands and run environment

--- claude/claude/run.sh
+++ claude/claude/run.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -eu
+
 user=$(id -u -n)
+
+workspace="${HOME}/Workspace"
+github="${HOME}/Github"
+
 cfgdir="${HOME}/Workspace/docker/dockerfiles/claude"
 install -v -d -m 0750 "${cfgdir}"
 install -v -d -m 0750 "${cfgdir}/config"
-if ! test -s "${cfgdir}/config/claude.json"; then
-	touch "${cfgdir}/config/claude.json"
+
+if ! test -s "${cfgdir}/claude.json"; then
+	touch "${cfgdir}/claude.json"
 fi
-slug="$(echo -n "$(basename "${PWD}")" | tr '[:space:]' '-')"
+
 exec docker run -it --rm -u "${user}" \
-	--name "claude-${slug}" \
-	--hostname "claude-${slug}.debian.local" \
+	--name "claude-${user}" \
+	--hostname "claude.debian.local" \
 	-e "TERM=${TERM}" \
 	-v "${cfgdir}/config:/home/${user}/.claude" \
 	-v "${cfgdir}/claude.json:/home/${user}/.claude.json" \
-	-v "${PWD}:/home/${user}/workspace" \
-	--workdir "/home/${user}/workspace" \
+	-v "${workspace}:/home/${user}/workspace:ro" \
+	-v "${github}:/home/${user}/github:ro" \
+	--workdir "/home/${user}" \
 	jcroots/claude
