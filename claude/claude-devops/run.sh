015290eb10ea2010fc48ae535541d2d96e490803
Jeremías <jeremias.rootstrap@gmail.com>
2026-03-23 14:18:24 -0300

    claude devops install summarize

--- claude/claude-devops/run.sh
+++ claude/claude-devops/run.sh
@@ -11,9 +11,12 @@ install -v -d -m 0750 "${workspace}"
 install -v -d -m 0750 "${github}"
 install -v -d -m 0750 "${temp}"
 
-cfgdir="${HOME}/Workspace/docker/dockerfiles/claude"
+install -v -d -m 0750 "${HOME}/Docker"
+
+cfgdir="${HOME}/Docker/dockerfiles/claude/config"
 install -v -d -m 0750 "${cfgdir}"
-install -v -d -m 0750 "${cfgdir}/config"
+install -v -d -m 0750 "${cfgdir}/claude"
+install -v -d -m 0750 "${cfgdir}/summarize"
 
 if ! test -s "${cfgdir}/claude.json"; then
 	touch "${cfgdir}/claude.json"
@@ -23,8 +26,9 @@ exec docker run -it --rm -u "${user}" \
 	--name "claude-devops-${user}" \
 	--hostname "claude-devops.debian.local" \
 	-e "TERM=${TERM}" \
-	-v "${cfgdir}/config:/home/${user}/.claude" \
+	-v "${cfgdir}/claude:/home/${user}/.claude" \
 	-v "${cfgdir}/claude.json:/home/${user}/.claude.json" \
+	-v "${cfgdir}/summarize:/home/${user}/.summarize" \
 	-v "${workspace}:/home/${user}/workspace:ro" \
 	-v "${github}:/home/${user}/github" \
 	-v "${temp}:/home/${user}/temp" \
